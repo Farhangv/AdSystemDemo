3ef68a536620e3ff12ae157ffc25c4f521ee4a11
Farhang Vafa <farhangv@ymail.com>
2018-05-19 13:32:24 +0430

    Init

---
+++ AdSystem.ClientSideTheme/Scripts/custom.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $('select[multiple]').select2({
+        dir: 'rtl'
+    });
+});
---
+++ AdSystem.ClientSideTheme/Search_Aciton.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdSystem.ClientSideTheme
+{
+    public partial class Search_Aciton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var query = Request.QueryString["query"];
+            result.InnerHtml = query;
+        }
+    }
+}
