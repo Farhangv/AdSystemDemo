2f5fe80fc8d187f04fbb1c6075d313f42bee6691
Farhang Vafa <farhangv@ymail.com>
2018-06-18 13:46:16 +0430

    Revert "Session37-Revert"
    This reverts commit a973b43ae0436d2c3aec6e566fe6ee80c5abda4a.

--- AdSystem.Models/Ad.cs
+++ AdSystem.Models/Ad.cs
@@ -9,20 +9,31 @@ namespace AdSystem.Models
 {
     public class Ad
     {
+        [Display(Name = "شناسه")]
         public int Id { get; set; }
         [MaxLength(100)]
+        [Display(Name = "عنوان")]
         public string Title { get; set; }
+        [Display(Name = "مساحت")]
         public int? Area { get; set; }
+        [Display(Name = "سن بنا")]
         public int? Age { get; set; }
+        [Display(Name = "تعداد واحد در طبقه")]
         public int? UnitCount { get; set; }
+        [Display(Name = "امکانات")]
         public virtual ICollection<Feature> Features { get; set; }
         [MaxLength(150)]
+        [Display(Name = "عکس")]
         public string ThumbnailPhotoPath { get; set; }
+        [Display(Name = "رسانه ها")]
         public virtual ICollection<Media> Media { get; set; }
+        [Display(Name = "دسته بندی")]
         public virtual Category Category { get; set; }
+        [Display(Name = "دسته بندی")]
         public int? CategoryId { get; set; }
 
         [NotMapped]
+        [Display(Name = "قیمت کل")]
         public virtual string TotalPriceText { get { return "۰"; } }
 
     }
--- AdSystem.Models/SaleAd.cs
+++ AdSystem.Models/SaleAd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace AdSystem.Models
 {
     public class SaleAd:Ad
     {
+        [Display(Name = "قیمت هر متر مربع")]
         public int? PricePerUnit { get; set; }
 
     }
